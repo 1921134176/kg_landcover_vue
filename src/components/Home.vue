dfff329ab05645251a9dce6e40305eadd957b827
ORANGE <1921134176@qq.com>
2021-12-31 14:33:15 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    家目录
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
