b95dce27662759ba3a1c360f2d8ea566904ce191
ORANGE <1921134176@qq.com>
2022-01-05 17:28:20 +0800

    完成系统登录功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,52 @@
 <template>
   <div>
-    家目录
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="3" disabled>消息中心</el-menu-item>
+          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-
+.el-header,
+.el-footer {
+  background-color: #fdfdfd;
+  color: #333;
+  text-align: center;
+  line-height: 100px;
+}
+.el-menu {
+  display: flex;
+  justify-content: flex-end;
+}
+.el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', redirect: '/Home' }, // 路由重定向
+  { path: '/', redirect: '/Login' }, // 路由重定向
+  { path: '/Login', component: Login },
   { path: '/Home', component: Home }
 ]
 
